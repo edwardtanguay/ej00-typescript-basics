1b76869d8c8e808bf8d1760af540a9152737d177
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-12 16:04:28 +0200

    shows title/authors

--- src/components/Books.tsx
+++ src/components/Books.tsx
@@ -1,14 +1,38 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
+interface IBook {
+	title: string;
+	authors: string[];
+}
+
 export const Books = () => {
-	const [books, setBooks] = useState([]);
+	const [books, setBooks] = useState<IBook[]>([]);
 
 	const url = 'https://gutendex.com/books/?search=berlin';
 
+	const getAuthors = (rawAuthors:any) => {
+		const authors: string[] = [];
+		rawAuthors.forEach((rawAuthor:any) => {
+			const author:string = rawAuthor.name;
+			authors.push(author);
+		})
+		return authors;
+	}
+
 	useEffect(() => {
 		(async () => {
-			const _books = (await axios.get(url)).data.results;
+			const _rawBooks = (await axios.get(url)).data.results;
+
+			const _books: IBook[] = [];
+			_rawBooks.forEach((rawBook: IBook) => {
+				const book: IBook = {
+					title: rawBook.title === undefined ? '(NO TITLE)' : rawBook.title,
+					authors: getAuthors(rawBook.authors), 
+				};
+				_books.push(book);
+			});
+
 			setBooks(_books);
 		})();
 	}, []);
@@ -16,7 +40,14 @@ export const Books = () => {
 	return (
 		<>
 			<h2>There are {books.length} books</h2>
-			<p>nnn</p>
+			{books.map((book) => {
+				return (
+					<div className="book">
+						<div className="title">{book.title}</div>
+						<div className="authors">{book.authors.join(',')}</div>
+					</div>
+				);
+			})}
 		</>
 	);
 };
